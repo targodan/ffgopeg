14c3de8da826c7fafb521479577de42f6f4d8f20
Luca Corbatto <luca@corbatto.de>
2016-10-11 11:06:43 +0200

    Reworked swscale

--- swscale/swscale.go
+++ swscale/swscale.go
@@ -26,42 +26,61 @@ type (
 	PixelFormat C.enum_AVPixelFormat
 )
 
-//Return the LIBSWSCALE_VERSION_INT constant.
-func SwscaleVersion() uint {
+// Version returns the LIBSWSCALE_VERSION_INT constant.
+//
+// C-Function:
+//
+// C-Function: swscale_version
+func Version() uint {
 	return uint(C.swscale_version())
 }
 
-//Return the libswscale build-time configuration.
-func SwscaleConfiguration() string {
+// Configuration returns the libswscale build-time configuration.
+//
+// C-Function: GoString
+func Configuration() string {
 	return C.GoString(C.swscale_configuration())
 }
 
-//Return the libswscale license.
-func SwscaleLicense() string {
+// License returns the libswscale license.
+//
+// C-Function: GoString
+func License() string {
 	return C.GoString(C.swscale_license())
 }
 
-//Return a pointer to yuv<->rgb coefficients for the given colorspace suitable for sws_setColorspaceDetails().
-func SwsGetcoefficients(c int) *int {
+// Coefficients returns a pointer to yuv<->rgb coefficients for the given colorspace suitable for sws_setColorspaceDetails().
+//
+// C-Function: sws_getCoefficients
+func Coefficients(c int) *int {
 	return (*int)(unsafe.Pointer(C.sws_getCoefficients(C.int(c))))
 }
 
-//Return a positive value if pix_fmt is a supported input format, 0 otherwise.
-func SwsIssupportedinput(p PixelFormat) int {
-	return int(C.sws_isSupportedInput((C.enum_AVPixelFormat)(p)))
+// IsSupportedInput returns true if pix_fmt is a supported input format,false otherwise.
+//
+// C-Function: sws_isSupportedInput
+func IsSupportedInput(p PixelFormat) bool {
+	return int(C.sws_isSupportedInput((C.enum_AVPixelFormat)(p))) != 0
 }
 
-//Return a positive value if pix_fmt is a supported output format, 0 otherwise.
-func SwsIssupportedoutput(p PixelFormat) int {
-	return int(C.sws_isSupportedOutput((C.enum_AVPixelFormat)(p)))
+// IsSupportedOutput returns true if pix_fmt is a supported output format, false otherwise.
+//
+// C-Function: sws_isSupportedOutput
+func IsSupportedOutput(p PixelFormat) bool {
+	return int(C.sws_isSupportedOutput((C.enum_AVPixelFormat)(p))) != 0
 }
 
-func SwsIssupportedendiannessconversion(p PixelFormat) int {
-	return int(C.sws_isSupportedEndiannessConversion((C.enum_AVPixelFormat)(p)))
+// IsSupportedEndiannessConversion returns true if endianess conversion is supported.
+//
+// C-Function: sws_isSupportedEndiannessConversion
+func IsSupportedEndiannessConversion(p PixelFormat) bool {
+	return int(C.sws_isSupportedEndiannessConversion((C.enum_AVPixelFormat)(p))) != 0
 }
 
-////Scale the image slice in srcSlice and put the resulting scaled slice in the image in dst.
-func SwsScale(ctxt *Context, src *uint8, str int, y, h int, d *uint8, ds int) int {
+// Scale scales the image slice in srcSlice and put the resulting scaled slice in the image in dst.
+//
+// C-Function: sws_scale
+func (ctxt *Context) Scale(src *uint8, str int, y, h int, d *uint8, ds int) int {
 	cctxt := (*C.struct_SwsContext)(unsafe.Pointer(ctxt))
 	csrc := (*C.uint8_t)(unsafe.Pointer(src))
 	cstr := (*C.int)(unsafe.Pointer(&str))
@@ -70,13 +89,19 @@ func SwsScale(ctxt *Context, src *uint8, str int, y, h int, d *uint8, ds int) in
 	return int(C.sws_scale(cctxt, &csrc, cstr, C.int(y), C.int(h), &cd, cds))
 }
 
-func SwsSetcolorspacedetails(ctxt *Context, it *int, sr int, t *int, dr, b, c, s int) int {
+// SetColorspaceDetails sets the colorspace details.
+//
+// C-Function: sws_setColorspaceDetails
+func (ctxt *Context) SetColorspaceDetails(it *int, sr int, t *int, dr, b, c, s int) int {
 	cit := (*C.int)(unsafe.Pointer(it))
 	ct := (*C.int)(unsafe.Pointer(t))
 	return int(C.sws_setColorspaceDetails((*C.struct_SwsContext)(ctxt), cit, C.int(sr), ct, C.int(dr), C.int(b), C.int(c), C.int(s)))
 }
 
-func SwsGetcolorspacedetails(ctxt *Context, it, sr, t, dr, b, c, s *int) int {
+// ColorspaceDetails returns the colorspace details.
+//
+// C-Function: sws_getColorspaceDetails
+func ColorspaceDetails(ctxt *Context, it, sr, t, dr, b, c, s *int) int {
 	cit := (**C.int)(unsafe.Pointer(it))
 	csr := (*C.int)(unsafe.Pointer(sr))
 	ct := (**C.int)(unsafe.Pointer(t))
@@ -87,25 +112,37 @@ func SwsGetcolorspacedetails(ctxt *Context, it, sr, t, dr, b, c, s *int) int {
 	return int(C.sws_getColorspaceDetails((*C.struct_SwsContext)(ctxt), cit, csr, ct, cdr, cb, cc, cs))
 }
 
-func SwsGetdefaultfilter(lb, cb, ls, cs, chs, cvs float32, v int) *Filter {
+// DefaultFilter returns the default filter.
+//
+// C-Function: sws_getDefaultFilter
+func DefaultFilter(lb, cb, ls, cs, chs, cvs float32, v int) *Filter {
 	return (*Filter)(unsafe.Pointer(C.sws_getDefaultFilter(C.float(lb), C.float(cb), C.float(ls), C.float(cs), C.float(chs), C.float(cvs), C.int(v))))
 }
 
-func SwsFreefilter(f *Filter) {
+// Free frees the filter.
+//
+// C-Function: sws_freeFilter
+func (f *Filter) Free() {
 	C.sws_freeFilter((*C.struct_SwsFilter)(f))
 }
 
-//Convert an 8-bit paletted frame into a frame with a color depth of 32 bits.
-func SwsConvertpalette8topacked32(s, d *uint8, px int, p *uint8) {
+// ConvertPalette8ToPacked32 converts an 8-bit paletted frame into a frame with a color depth of 32 bits.
+//
+// C-Function: sws_convertPalette8ToPacked32
+func ConvertPalette8ToPacked32(s, d *uint8, px int, p *uint8) {
 	C.sws_convertPalette8ToPacked32((*C.uint8_t)(s), (*C.uint8_t)(d), C.int(px), (*C.uint8_t)(p))
 }
 
-//Convert an 8-bit paletted frame into a frame with a color depth of 24 bits.
-func SwsConvertpalette8topacked24(s, d *uint8, px int, p *uint8) {
+// ConvertPalette8ToPacked24 converts an 8-bit paletted frame into a frame with a color depth of 24 bits.
+//
+// C-Function: sws_convertPalette8ToPacked24
+func ConvertPalette8ToPacked24(s, d *uint8, px int, p *uint8) {
 	C.sws_convertPalette8ToPacked24((*C.uint8_t)(s), (*C.uint8_t)(d), C.int(px), (*C.uint8_t)(p))
 }
 
-//Get the Class for swsContext.
-func SwsGetClass() *Class {
+// GetClass gets the Class for swsContext.
+//
+// C-Function: sws_get_class
+func GetClass() *Class {
 	return (*Class)(C.sws_get_class())
 }
